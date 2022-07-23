a84998543912421465e0588e2eba4520de91e485
kodenart <mjuanvitorp@gmail.com>
2022-07-23 14:23:45 -0300

    Iniciando o projeto

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+    ],
+    "rules": {
+        "no-unused-vars": ["warn", { "argsIgnorePattern": "next" }],
+        "no-console": "off",
+        "func-names": "off",
+        "semi": [2, "always"]
+    }
+};
