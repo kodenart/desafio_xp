503980a1976c8e90133a40b2bae2803eb368c704
kodenart <mjuanvitorp@gmail.com>
2022-07-24 21:39:19 -0300

    chore: some new dependecies

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import express from 'express';
 import { DBConnection } from './config/db';
 import { ErrorHandler } from './middlewares/ErrorHandler';
 import Router from './routers';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
